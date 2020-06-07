bdae261b8dc7f7dce4ec2d63b113ca33932d8076
Femi <iamkeyiano@gmail.com>
2020-06-07 01:20:27 +0100

    added fep routing

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const net = require('net');
 const port = 43666;
 const host = '0.0.0.0';
+const fepHost = '10.154.0.12';
+const fepPort = '49155';
 
 const server = net.createServer();
+const client = new net.Socket();
+
 server.listen(port, host, () => {
     console.log('TCP Server is running on port ' + port + '.');
 });
@@ -14,9 +18,22 @@ server.on('connection', function(sock) {
     sockets.push(sock);
 
     sock.on('data', function(data) {
+        client.connect(fepPort, fepHost, function() {
+            console.log("Connected to patricia pay fep");
+            client.write(data);
+        })
+        client.on('data', function(data) {
+            console.log("Patricia Pay FEP: " + data);
+        })
+        client.on('close', function() {
+            console.log("Patricia Pay FEP connection closed");
+        });
+
         console.log('DATA ' + sock.remoteAddress + ': ' + data);
+
         // Write the data back to all the connected, the client will receive it as data from the server
         sockets.forEach(function(sock, index, array) {
+
             sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
         });
     });
