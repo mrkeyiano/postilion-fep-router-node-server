78fea8a1b2475b1ff661f5f57c5375d844749201
Femi <iamkeyiano@gmail.com>
2020-06-25 13:45:34 +0100

    random variable name for multiple instance of sock test

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const fepHost = '10.154.0.12';
 const fepPort = '43666';
 var intervalConnect = false;
 var timeout = 0;
+var i=0;
 
 var demo_data = "�0421F23E0495ABE081200000004204000022165321550400409731312000000000000000061510555673737716504008092304042000000C00000000C000000000653215506111111245321550400409731=23042210000032982606822110300022100000000000022HAGGAI  MFB  ATM  GARKI     ABUJA   LANG56600440210100003298260020073737706151055560000053215500000111111000000000000000000000000C00000000C0000000010002395301701551120151114C0020013646420144000000000100003298260PAT2src     PAT2snk     737377737377UBPGroup    301000032982602020061501252218Postilion:MetaData278211MediaTotals111212MediaBatchNr111217AdditionalEmvTags111214AdditionalInfo111211MediaTotals3116<MediaTotals><Totals><Amount>0</Amount><Currency>000</Currency><MediaClass>Cards</MediaClass></Totals></MediaTotals>212MediaBatchNr173736441217AdditionalEmvTags3500<AdditionalEmvTags><EmvTag><TagId>50</TagId><TagValue>4465626974204D617374657243617264</TagValue></EmvTag><EmvTag><TagId>81</TagId><TagValue>0000C350</TagValue></EmvTag><EmvTag><TagId>9F4C</TagId><TagValue>0000000000000000</TagValue></EmvTag><EmvTag><TagId>9F45</TagId><TagValue>0000</TagValue></EmvTag><EmvTag><TagId>5F36</TagId><TagValue>00</TagValue></EmvTag><EmvTag><TagId>5F34</TagId><TagValue>00</TagValue></EmvTag><EmvTag><TagId>9B</TagId><TagValue>6000</TagValue></EmvTag></AdditionalEmvTags>214AdditionalInfo3447<AdditionalInfo><Transaction><OpCode>AGABHAIA</OpCode><BufferB>08136900929</BufferB><BufferC>1774691015</BufferC><CfgExtendedTrxType>8505</CfgExtendedTrxType><CfgReceivingInstitutionIDCode>62805112345</CfgReceivingInstitutionIDCode></Transaction><Download><ATMConfigID>5006</ATMConfigID><AtmAppConfigID>5006</AtmAppConfigID><LoadsetGroup>FEP Wincor EMV</LoadsetGroup><DownloadApp>QT3_DOWNLOAD_WESTERNUNION</DownloadApp></Download></AdditionalInfo>07PAT2snk";
 
@@ -56,18 +57,19 @@ server.on('connection', function(sock) {
                         //generate random variables name
 
 
-                        let rand_var = "conn_" + makeid(5) + new Date().getTime();
-                        const fep = 'fepClient';
-                        rand_var[fep] = new net.Socket();
+                        let rand_var = "fepClient";
+                        i = i+1;
+
+                        rand_var[i] = new net.Socket();
                         console.log("RANDOM VaRIABLE NAME:" + rand_var[fep]);
 
-                    rand_var[fep].connect({
+                    rand_var[i].connect({
                             port: fepPort,
                             host: fepHost,
                         });
 
 
-                    rand_var[fep].write(received.toString() +"\n");
+                    rand_var[i].write(received.toString() +"\n");
                         console.log(data_id +": data sent to fep server, waiting for response.");
 
 
@@ -81,13 +83,13 @@ server.on('connection', function(sock) {
 
 
 
-        rand_var[fep].on('connect', function() {
+        rand_var[i].on('connect', function() {
             console.log(data_id +": connected to patricia pay fep running on ip " + fepHost + " and port " +fepPort);
         });
 
         //wait for response and forward back to postbridge
 
-        rand_var[fep].on('data', function(data) {
+        rand_var[i].on('data', function(data) {
             console.log(data_id +": patricia pay fep server responded to request");
             console.log(data_id +": forwarding data to unitybank postbridge");
             //write data to unitybank postbridge
@@ -103,7 +105,7 @@ server.on('connection', function(sock) {
 
 
             if (data.toString().endsWith('exit')) {
-                rand_var[fep].destroy();
+                rand_var[i].destroy();
 
             }
 
@@ -113,28 +115,28 @@ server.on('connection', function(sock) {
         });
 
         //catch errors connecting to fep
-        rand_var[fep].on('error', function(ex) {
+        rand_var[i].on('error', function(ex) {
 
 
             console.log(data_id +": error connecting to fep client: " +ex);
 
-            rand_var[fep].destroy();
+            rand_var[i].destroy();
 
 
         });
 
 
-        rand_var[fep].on('close', function() {
+        rand_var[i].on('close', function() {
 
             console.log(data_id +": fep server connection closed");
 
-            rand_var[fep].removeAllListeners();
-            rand_var[fep].destroy();
+            rand_var[i].removeAllListeners();
+            rand_var[i].destroy();
 
 
         });
 
-        rand_var[fep].on('end', function() {
+        rand_var[i].on('end', function() {
 
             console.log(data_id +": fep server connection ended");
 
