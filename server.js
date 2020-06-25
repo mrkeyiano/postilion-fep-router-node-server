403c65125c09eeceaa96b70a87063cb08fc70449
Femi <iamkeyiano@gmail.com>
2020-06-25 14:18:28 +0100

    random variable name for multiple instance of sock test

--- server.js
+++ server.js
@@ -105,6 +105,7 @@ server.on('connection', function(sock) {
             //check if data ends with or contains new line
             if (data.toString().indexOf("\n")===-1) {
                 //do nothing
+                this[i].destroy();
             } else {
                 this[i].destroy();
             }
