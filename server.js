413e8aaa61d77bac2d51685965e22d93e191d929
Femi <iamkeyiano@gmail.com>
2020-06-25 14:23:35 +0100

    random variable name for multiple instance of sock test

--- server.js
+++ server.js
@@ -105,9 +105,10 @@ server.on('connection', function(sock) {
             //check if data ends with or contains new line
             if (data.toString().indexOf("\n")===-1) {
                 //do nothing
-                [i].destroy();
+               // [i].destroy();
             } else {
-                [i].destroy();
+               // [i].destroy();
+
             }
 
             //close fepClient connection
